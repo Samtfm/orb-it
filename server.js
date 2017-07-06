6c6987107eb0d1733d40465a57801100fe5793a4
Grey Keith <r.grey.keith@gmail.com>
2017-07-06 14:57:02 -0700

    Build Server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+const port = process.env.API_PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/vr/index.html');
+})
+
+app.use('/api', router);
+
+router.get('/', function(req, res) {
+  res.json({ message: 'API Initialized' });
+});
+
+
+app.listen(port, function() {
+  console.log(`api running on port ${port}`)
+})
