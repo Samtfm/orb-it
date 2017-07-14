4ea7a0e9ecd84841a92fee7156718e705201e8c2
Samtfm <sfmemail@gmail.com>
2017-07-14 16:33:13 -0700

    create uploadImage component

---
+++ frontend/components/upload_image.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+  VrButton,
+} from 'react-vr';
+
+export default class UploadImage extends React.Component {
+
+
+  render() {
+    return (
+      <View>
+        <Text>Upload Image</Text>
+      </View>
+    );
+  }
+
+}
