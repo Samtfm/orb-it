11e1a9b185648406cfdfc0d2b9b025ef14a4bc4a
Samtfm <sfmemail@gmail.com>
2017-07-25 15:18:25 -0700

    distance-rotation model of sphere layout

--- frontend/components/orb_index.js
+++ frontend/components/orb_index.js
@@ -3,17 +3,54 @@ import { AppRegistry, asset, Pano, Text, View, Sphere } from 'react-vr';
 
 const indexStyle = {
   position: 'absolute',
-  transform: [{translate: [-2.5, 0, -5]}],
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  width: 5,
+  // transform: [{translate: [-2.5, 0, 0]}],
+  // flexDirection: 'row',
+  // justifyContent: 'space-between',
+  // width: 10,
   // alignItems: 'stretch',
 };
 
+
+
+class OrbThumb extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      distance: props.distance,
+      rotationY: props.rotationY
+    };
+  }
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      distance: nextProps.distance,
+      rotationY: nextProps.rotationY
+    });
+  }
+  render(){
+    const style = {
+      position: 'absolute',
+      transform: [
+        {rotateY: this.state.rotationY+90},
+        {translate: [this.state.distance, 0, 0]}],
+    };
+    return (
+      <View style={style}>
+        <Sphere
+          radius={1}
+          widthSegments={20}
+          heightSegments={12}
+        />
+      </View>
+    );
+  }
+}
 class OrbIndex extends Component {
   render() {
     return (
       <View style={indexStyle}>
+        <OrbThumb distance='5' rotationY={0}/>
+          <OrbThumb distance='5' rotationY={50}/>
+            <OrbThumb distance='5' rotationY={80}/>
         <Sphere
           radius={0.5}
           widthSegments={20}
