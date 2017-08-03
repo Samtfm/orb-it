419c63b341f0b637208c90d8536e3a7c9501c1b3
Samtfm <sfmemail@gmail.com>
2017-08-03 14:50:05 -0700

    remove buttons from index.vr.js

--- index.vr.js
+++ index.vr.js
@@ -20,39 +20,6 @@ export default class OrbIt extends React.Component {
         <OrbIndex/>
         <Navigation/>
         <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: this.state.bg,
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}
-          onEnter={() => this.setState({bg: 'blue'})}
-          onExit={() => this.setState({bg: 'red'})}>
-          {this.state.message}
-        </Text>
-        <VrButton
-          onClick={() => this.changeMessage()}>
-          <Text
-            style={{
-              backgroundColor: 'grey',
-              fontSize: 0.8,
-              fontWeight: '400',
-              layoutOrigin: [0.5, 0.5],
-              paddingLeft: 0.2,
-              paddingRight: 0.2,
-              textAlign: 'center',
-              textAlignVertical: 'center',
-              transform: [{translate: [0, 0, -3]}],
-            }}>
-            Change Message
-          </Text>
-        </VrButton>
       </View>
     );
   }
