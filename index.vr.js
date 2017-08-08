cd32ae77b7ebd040eae66c04ce50f3bb2f51715c
Samtfm <sfmemail@gmail.com>
2017-08-07 20:50:30 -0700

    add memory router

--- index.vr.js
+++ index.vr.js
@@ -8,19 +8,30 @@ import {
   View,
   VrButton,
 } from 'react-vr';
+import { Match, Redirect, MemoryRouter as Router } from 'react-router';
+// import { HashRouter } from 'react-router-dom';
 
 
 import OrbIndex from './frontend/components/thumbnails/orb_index';
 import Navigation from './frontend/components/navigation/navigation';
+// <route component=OrbView path='/orbs/orb-id' props='orb-id'></route>
 
 export default class OrbIt extends React.Component {
+
   render() {
+    console.log(Match);
     return (
-      <View>
-        <OrbIndex/>
-        <Navigation/>
-        <Pano source={asset('chess-world.jpg')}/>
-      </View>
+      <Router>
+        <View>
+          <Match pattern='/' exact={true} render={() => (
+            <Redirect to={`/orbs/{6}`} />
+          )} />
+        <Match pattern='/orbs' component={OrbIndex} />
+
+          <Navigation/>
+          <Pano source={asset('chess-world.jpg')}/>
+    </View>
+  </Router>
     );
   }
 }
